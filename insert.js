94e4577442da77a734b1fe294c56afb46914807a
jaga <kandros5591@gmail.com>
2017-02-05 19:23:56 +0100

    prevent race condition when table not already crated

--- insert.js
+++ insert.js
@@ -4,10 +4,13 @@ const db = new sqlite3.Database(db_name);
 
 const title = process.argv[2];
 
-db.run(`CREATE TABLE IF NOT EXISTS ${table_name} (title TEXT)`);
-db.run(`INSERT INTO ${table_name} VALUES (?)`, title);
-
-db.each(`SELECT rowid AS id, title FROM ${table_name}`, function(err, row) {
-    console.log(row.id + ": " + row.title);
+db.serialize(() => {
+    db.run(`CREATE TABLE IF NOT EXISTS ${table_name} (title TEXT)`);
+    db.run(`INSERT INTO ${table_name} VALUES (?)`, title);
+    
+    db.each(`SELECT rowid AS id, title FROM ${table_name}`, function(err, row) {
+        console.log(row.id + ": " + row.title);
+    });
 });
+
 db.close();
